b97add4f48787831310cff6bcc4fd6d650a83fb0
wangyunfei <wangyunfei@qiyi.com>
2017-04-12 15:47:53 +0800

    去除无用代码

--- problems/algorithms/165.compare-version-numbers.js
+++ problems/algorithms/165.compare-version-numbers.js
@@ -27,59 +27,7 @@ var compareVersion = function (version1, version2) {
     }
 };
 
-// write('algorithms: 165. Compare Version Numbers', 'title');
-// write(compareVersion('1', '1.1'));
-// write(compareVersion('1.0.1', '1'));
-// write(compareVersion('2.5', '2.3'));
-
-
-
-var longestPalindrome = function (s) {
-    var length = s.length;
-    if (length <= 1) {
-        return length;
-    } 
-    var max = 0, cur;
-    for(var i = 0; i < length; i++) {
-        for(var j = i + 1; j <= length; j++) {
-            cur = getPalindromeLen(s.substring(i, j));
-            max = max > cur ? max : cur;
-        }
-    }
-    return max;
-}
-
-function getPalindromeLen(str) {
-    if (str === str.split('').reverse().join('')) {
-        return str.length;
-    }
-    return 0;
-}
-
-// function getPalindromeLen(str) {
-//     var length = str.length;
-//     if (length <= 1) {
-//         return length;
-//     } 
-//     var right, left;
-//     if (length % 2 === 0) {
-//         right = parseInt(length / 2);
-//         left = right - 1;
-//     } else {
-//         right = parseInt(length / 2) + 1;
-//         left = right - 2;
-//     }
-//     var isHuiwen = true;
-//     while (right < length && left > -1) {
-//         if (str[right++] !== str[left--]) {
-//             isHuiwen = false;
-//             break;
-//         }
-//     }
-//     return isHuiwen ? str.length : 0;
-// }
-
-write(longestPalindrome('a'));
-write(longestPalindrome("zyaayz"));
-write(longestPalindrome("baablkj12345432133d"));
-
+write('algorithms: 165. Compare Version Numbers', 'title');
+write(compareVersion('1', '1.1'));
+write(compareVersion('1.0.1', '1'));
+write(compareVersion('2.5', '2.3'));
