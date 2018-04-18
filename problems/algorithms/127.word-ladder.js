343f70d0de7451e244697d6b3728c65fd214dce2
wangyunfei <wangyunfei@qiyi.com>
2018-04-18 17:54:26 +0800

    complete 127

--- problems/algorithms/127.word-ladder.js
+++ problems/algorithms/127.word-ladder.js
@@ -11,6 +11,7 @@
  * @param {string[]} wordList
  * @return {number}
  */
+// set 双端
 var ladderLength = function (beginWord, endWord, wordList) {
   wordList = new Set(wordList)
   if (wordList.size === 0 || !wordList.has(endWord)) {
@@ -21,10 +22,7 @@ var ladderLength = function (beginWord, endWord, wordList) {
 
   let beginSet = new Set([beginWord]),
     endSet = new Set([endWord]),
-    visited = new Set()
-
-  let len = 1,
-    strLen = beginWord.length
+    len = 1
 
   while (beginSet.size > 0 && endSet.size > 0) {
     if (beginSet.size > endSet.size) {
@@ -32,7 +30,6 @@ var ladderLength = function (beginWord, endWord, wordList) {
       beginSet = endSet
       endSet = tmp
     }
-
     let temp = new Set()
     for (let word of beginSet.keys()) {
       for (let i = 0; i < word.length; i++) {
@@ -47,14 +44,134 @@ var ladderLength = function (beginWord, endWord, wordList) {
             return len + 1
           }
 
-          if (!visited.has(newWord) && wordList.has(newWord)) {
+          if (wordList.has(newWord)) {
             temp.add(newWord)
-            visited.add(newWord)
+            wordList.delete(newWord)
           }
         }
       }
     }
     beginSet = temp
+    len++
+  }
+  return 0
+}
+// set 单端
+var ladderLength1 = function (beginWord, endWord, wordList) {
+  wordList = new Set(wordList)
+  if (wordList.size === 0 || !wordList.has(endWord)) {
+    return 0
+  }
+
+  wordList.delete(endWord)
+
+  let beginSet = new Set([beginWord]),
+    len = 1
+
+  while (beginSet.size > 0) {
+    let temp = new Set()
+    for (let word of beginSet.keys()) {
+      for (let i = 0; i < word.length; i++) {
+        let former = i == 0 ? '' : word.slice(0, i)
+        let after = word.slice(i + 1)
+
+        for (let j = 0; j < 26; j++) {
+          let letter = String.fromCharCode(97 + j)
+          let newWord = former + letter + after
+
+          if (newWord === endWord) {
+            return len + 1
+          }
+
+          if (wordList.has(newWord)) {
+            temp.add(newWord)
+            wordList.delete(newWord)
+          }
+        }
+      }
+    }
+    beginSet = temp
+    len++
+  }
+  return 0
+}
+
+// 数组 单端
+var ladderLength2 = function (beginWord, endWord, wordList) {
+  if (wordList.length === 0 || wordList.indexOf(endWord) === -1) {
+    return 0
+  }
+
+  let begin = [beginWord],
+    len = 1
+
+  while (begin.length > 0) {
+    let temp = []
+    for (let word of begin) {
+      for (let i = 0; i < word.length; i++) {
+        let former = i == 0 ? '' : word.slice(0, i)
+        let after = word.slice(i + 1)
+
+        for (let j = 0; j < 26; j++) {
+          let letter = String.fromCharCode(97 + j)
+          let newWord = former + letter + after
+
+          if (newWord === endWord) {
+            return len + 1
+          }
+
+          let index = wordList.indexOf(newWord)
+          if (index > -1) {
+            temp.push(newWord)
+            wordList.splice(index, 1)
+          }
+        }
+      }
+    }
+    begin = temp
+    len++
+  }
+  return 0
+}
+// 数组 双端
+var ladderLength3 = function (beginWord, endWord, wordList) {
+  if (wordList.length === 0 || wordList.indexOf(endWord) === -1) {
+    return 0
+  }
+
+  let begin = [beginWord],
+    end = [endWord],
+    len = 1
+
+  while (begin.length > 0 && end.length > 0) {
+    if (begin.length > end.length) {
+      let tmp = begin
+      begin = end
+      end = tmp
+    }
+    let temp = []
+    for (let word of begin) {
+      for (let i = 0; i < word.length; i++) {
+        let former = i == 0 ? '' : word.slice(0, i)
+        let after = word.slice(i + 1)
+
+        for (let j = 0; j < 26; j++) {
+          let letter = String.fromCharCode(97 + j)
+          let newWord = former + letter + after
+
+          if (end.indexOf(newWord) > -1) {
+            return len + 1
+          }
+
+          let index = wordList.indexOf(newWord)
+          if (index > -1) {
+            temp.push(newWord)
+            wordList.splice(index, 1)
+          }
+        }
+      }
+    }
+    begin = temp
     len++
   }
   return 0
