22f0c9691f480bd86bf36720d2092efcbdeed7a0
geoffrey-ne <wyfzqx@gmail.com>
2022-05-23 09:25:54 +0800

    add

--- problems/algorithms/464.can-i-win.js
+++ problems/algorithms/464.can-i-win.js
@@ -5,13 +5,42 @@
  */
 
 /**
+ * 官方解答：记忆化搜索 + 状态压缩
+ * 
  * @param {number} maxChoosableInteger
  * @param {number} desiredTotal
  * @return {boolean}
  */
 var canIWin = function(maxChoosableInteger, desiredTotal) {
-
-}
+    const memo = new Map();
+    const dfs = (maxChoosableInteger, usedNumbers, desiredTotal, currentTotal) => {
+        if (!memo.has(usedNumbers)) {
+            let res = false;
+            for (let i = 0; i < maxChoosableInteger; i++) {
+                if (((usedNumbers >> i) & 1) === 0) {
+                    if (i + 1 + currentTotal >= desiredTotal) {
+                        res = true;
+                        break;
+                    }
+                    if (!dfs(maxChoosableInteger, usedNumbers | (1 << i), desiredTotal, currentTotal + i + 1)) {
+                        res = true;
+                        break;
+                    }
+                }
+            }
+            memo.set(usedNumbers, res);
+        }
+        return memo.get(usedNumbers);
+    }
+    // 边界
+    if ((1 + maxChoosableInteger) * (maxChoosableInteger) / 2 < desiredTotal) {
+        return false;
+    }
+    return dfs(maxChoosableInteger, 0, desiredTotal, 0);
+};
 
 write('algorithms: 464. Can I Win', 'title')
-write(canIWin(10, 11));  // false
+// write(canIWin(10, 11));  // false
+write(canIWin(15, 100));  // false
+
+// tags: 记忆化搜索；状态压缩
